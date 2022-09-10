d9766c05ce26c02d49257931f7c3c0de37484210
bsef19a522 <bsef19a522@pucit.edu.pk>
2022-09-10 23:33:00 +0500

    Update and Delete

---
+++ app/src/main/java/com/example/curd/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.curd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonAdd, buttonViewAll, buttonDelete, buttonUpdate;
+    EditText editName, editRollNumber;
+    Switch switchIsActive;
+    ListView listViewStudent;
+    TextView searches;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonAdd = findViewById(R.id.buttonadd);
+        buttonViewAll = findViewById(R.id.buttonview);
+        buttonDelete = findViewById(R.id.buttondelete);
+        buttonUpdate = findViewById(R.id.buttonupdate);
+        editName = findViewById(R.id.editname);
+        editRollNumber = findViewById(R.id.editrollno);
+        switchIsActive = findViewById(R.id.switch1);
+        listViewStudent = findViewById(R.id.listview);
+        searches = findViewById(R.id.searches);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            StudentModel studentModel;
+
+            @Override
+            public void onClick(View v) {
+                try {
+                    studentModel = new StudentModel(editName.getText().toString(), Integer.parseInt(editRollNumber.getText().toString()), switchIsActive.isChecked());
+                    //Toast.makeText(MainActivity.this, studentModel.toString(), Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e){
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+                DBHelper dbHelper  = new DBHelper(MainActivity.this);
+                dbHelper.addStudent(studentModel);
+            }
+        });
+
+        buttonViewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbHelper = new DBHelper(MainActivity.this);
+                List<StudentModel> list = dbHelper.getAllStudents();
+                ArrayAdapter arrayAdapter;
+                arrayAdapter = new ArrayAdapter<StudentModel>
+                        (MainActivity.this, android.R.layout.simple_list_item_1,list);
+                listViewStudent.setAdapter(arrayAdapter);
+
+            }
+        });
+
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            StudentModel studentModel;
+
+            @Override
+            public void onClick(View v) {
+                if (!editRollNumber.getText().toString().isEmpty()) {
+                    DBHelper db = new DBHelper(MainActivity.this);
+                    boolean search = db.delete(Integer.parseInt(editRollNumber.getText().toString()));
+
+                    if(search){
+                        editName.setText("");
+                        editRollNumber.setText("");
+                        searches.setText("Deleted");
+                    }
+                    else{
+                        searches.setText("No record found");
+                    }
+                }
+                else{
+                    searches.setText("Please enter correct Rollno");
+                }
+            }
+        });
+
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            StudentModel studentModel;
+
+            @Override
+            public void onClick(View v) {
+                if (!editName.getText().toString().isEmpty() && !editRollNumber.getText().toString().isEmpty()) {
+                    DBHelper db = new DBHelper(MainActivity.this);
+                    boolean search = db.update(Integer.parseInt(editRollNumber.getText().toString()),editName.getText().toString());
+
+                    if(search){
+                        editName.setText("");
+                        editRollNumber.setText("");
+                        searches.setText("Updated");
+                    }
+                    else{
+                        searches.setText("No record found");
+                    }
+                }
+                else{
+                    searches.setText("Please enter Name and Rollno");
+                }
+            }
+        });
+    }
+}
